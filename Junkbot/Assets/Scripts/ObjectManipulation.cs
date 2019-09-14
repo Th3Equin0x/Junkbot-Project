3044018bf82404fdde10d0b7f7593ed961773b08
Th3Equin0x <sgruver22@gmail.com>
2019-09-13 23:55:52 -0500

    Added the scene for object manipulation, some objects, and some scripts

---
+++ Junkbot/Assets/Scripts/ObjectManipulation.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectManipulation : MonoBehaviour
+{
+    public Camera playerCam;
+    public GameObject selected;
+    public float rotationSpeed;
+    public bool leftClick;
+    public bool rightClick;
+
+    public float yaw;
+    public float pitch;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftClick = Input.GetMouseButton(0);
+        rightClick = Input.GetMouseButton(1);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                Debug.Log(hit.transform.gameObject.name);
+                selected = hit.transform.gameObject;
+            }
+        }
+
+        if (Input.GetMouseButton(1) && selected != null)
+        {
+            yaw += Input.GetAxis("Mouse X") * rotationSpeed * -1;
+            pitch += Input.GetAxis("Mouse Y") * rotationSpeed;
+
+            selected.transform.eulerAngles = new Vector3(pitch, yaw, 0);
+        }
+    }
+}
